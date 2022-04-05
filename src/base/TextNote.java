edddc0d223297cce99b05596e9ef6d697b37eeb5
chblaw <chblaw@connect.ust.hk>
2022-04-05 14:30:42 +0800

    lab 7

--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,13 +1,11 @@
 package base;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 
 public class TextNote extends Note{
